aefdff2590a990eb2665193baa88030b42cb9a82
charitochieng <charity254atieno@gmail.com>
2026-03-29 13:54:13 +0300

    feat: implement ant distribution load-balancing algorithm

--- internal/pathfinder.go
+++ internal/pathfinder.go
@@ -21,7 +21,7 @@ type node struct {
 	links map[link]empty
 }
 
-func initGraph(rooms []Room, tunnels []Tunnel) (alpha node, omega node)
+func initGraph(rooms []Room, tunnels []Tunnel) (alpha node, omega node) { return }
 
 // FindPaths returns a slice of routes, which is a slice of rooms from start to end.
-func FindPaths(ants int, rooms []Room, tunnels []Tunnel) [][]string
+func FindPaths(ants int, rooms []Room, tunnels []Tunnel) [][]string { return nil }
