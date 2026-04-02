cb37be18c15ef7a09b57d502af2ea375a8893b29
Ashomondi <adhiamboa745@gmail.com>
2026-04-02 10:10:24 +0300

    updated it return pathassignment instead of nil

--- internal/distributor.go
+++ internal/distributor.go
@@ -6,5 +6,32 @@ type PathAssignment struct {
 }
 
 func DistributeAnts(numAnts int, paths [][]string) []PathAssignment {
-	return nil
+	if len(paths) == 0 {
+		return []PathAssignment{}
+	}
+
+	assignments := make([]PathAssignment, len(paths))
+	for i, p := range paths {
+		assignments[i] = PathAssignment{
+			Path: p,
+			Ants: 0,
+		}
+	}
+
+	for i := 0; i < numAnts; i++ {
+		best := 0
+
+		for j := 1; j < len(assignments); j++ {
+			costBest := len(assignments[best].Path) + assignments[best].Ants
+			costJ := len(assignments[j].Path) + assignments[j].Ants
+
+			if costJ < costBest {
+				best = j
+			}
+		}
+
+		assignments[best].Ants++
+	}
+
+	return assignments
 }
